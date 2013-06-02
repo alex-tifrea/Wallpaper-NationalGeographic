c962709f7e3df16e3cfc602d68312c4af374627a
alex-tifrea <alex.tifrea93@gmail.com>
2013-06-02 10:41:50 +0300

    Script POD

---
+++ natgeo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#TODO: set the folder where you would like to download the
+cd ~/Pictures/
+mkdir National_Geographic_POD
+mkdir Saved_pictures_natgeo
+folder=~/Pictures/National_Geographic_POD
+
+cd $folder
+sudo rm index* *jpg
+POD="http://photography.nationalgeographic.com/photography/photo-of-the-day/?source=NavPhoPOD"
+wget -nc $POD -P $folder
+adr=$(ls | grep [^a])
+var=$(cat $adr | tr -s '\n' | grep -i 'go to the previous' -n | cut -d ':' -f1)
+
+wget -nc -P $folder $(cat $adr | tr -s '\n' | head -n $((var+1)) | tail -1 | cut -d "\"" -f2)
+
+rm index*
+
+PICTURE=$(ls *jpg)
+gsettings set org.gnome.desktop.background picture-uri file:///home/alex/Pictures/National_Geographic/$PICTURE
+
+if test $# -eq 1; then
+    if test $1 = "-s" -o $1 = "--save-image"; then
+        cp *.jpg ../Saved_pictures_natgeo/
+    else
+        echo "natgeo.sh: Invalid argument"
+        exit 1
+    fi
+fi
+
+if test $# -gt 1; then
+        echo "natgeo.sh: Too many arguments"
+        exit 2
+fi
+
+exit 0
